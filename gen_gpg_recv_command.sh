bc13235a0e2ebd2a5aadf5f454f4cc05720d683e
Daniel Krol <orblivion@gmail.com>
2014-01-17 23:27:06 -0800

    Just kidding, now I'm adding gen_gpg_recv_command.sh

---
+++ gen_gpg_recv_command.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# TODO - use the header, footer stuff to make this purdy
+
+# It seems it will not be happy to do this stuff while running. At very
+# least, it seems that if I do this while running, it needs to be
+# restarted after. So I'll just turn it off.
+sudo service sks stop
+
+sks dump 1000 /tmp/
+
+echo -n "" > /var/www/allkeys.txt
+
+echo -n "gpg --keyserver cryptoparty.com --recv-keys " >> /var/www/allkeys.txt
+# sed craziness: http://stackoverflow.com/a/1252191
+gpg /tmp/sks-dump-0000.pgp | grep pub | cut -b 12-19 | sed ':a;N;$!ba;s/\n/ /g' >> /var/www/allkeys.txt
+
+
+echo -n "\n########################################################################\n#\n# " >> /var/www/allkeys.txt
+date >> /var/www/allkeys.txt
+echo -n "#\n# " >> /var/www/allkeys.txt
+gpg /tmp/sks-dump-0000.pgp | sed ':a;N;$!ba;s/\n/\n# /g' >> /var/www/allkeys.txt
+
+# Restarting now that we've dumped it.
+sudo service sks start
